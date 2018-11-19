257da42d616d0c282c7e5e8a333ad7648dea76ae
apopielec <apopielec@interia.pl>
2018-11-19 02:47:23 +0100

    Fix and update temperature displaying when it has decimal values

--- src/temperature.py
+++ src/temperature.py
@@ -16,39 +16,43 @@ def sleeper(seconds):
 
 
 def get_temperature():
-    global temperature
+    global temperature_new
     response = requests.get('http://api.openweathermap.org/data/2.5/'
                             'weather?id=3081368&units=metric&appid=97b270e75c4525b3d0e79a2d1b635a2e')
     if response.status_code == requests.codes.ok:
         json_object = response.json()
-        temperature = json_object['main']['temp']
+        temperature_new = int(json_object['main']['temp'])
     else:
-        temperature = None
+        temperature_new = None
 
 
 def display_temperature():
-    global temperature
+    global temperature_current, temperature_new
     get_temperature()
 
-    # clear temperature display
-    for i in range(5, 9):
-        device.letter(0, i, ' ')
+    if not (temperature_new is None):
+        if temperature_new != temperature_current:
+            temperature_current = temperature_new
 
-    if not (temperature is None):
-        index = 8
+            # clear temperature display
+            for i in range(5, 9):
+                device.letter(0, i, ' ')
 
-        if temperature < 0:
-            index = 7
-            device.letter(0, 8, '-')
+            index = 8
+            if temperature_current < 0:
+                index = 7
+                device.letter(0, 8, '-')
 
-        if abs(temperature)/10 > 0:
-            device.letter(0, index, int(abs(temperature) / 10))    # Tens
-            index -= 1
+            if abs(temperature_current)/10 > 0:
+                device.letter(0, index, int(abs(temperature_current) / 10))    # Tens
+                index -= 1
 
-        device.letter(0, index, int(abs(temperature) % 10))  # Ones
-        device.letter(0, index-1, "*")
+            device.letter(0, index, int(abs(temperature_current) % 10))  # Ones
+            device.letter(0, index-1, "*")
     else:
         device.letter(0, 8, '-')
+        for i in range(5, 8):
+            device.letter(0, i, ' ')
 
     sleep_time = 900
     sleeper_thread = threading.Thread(target=sleeper, name="sleeper", args=(sleep_time,))
@@ -64,7 +68,8 @@ def run():
         time.sleep(1)
 
 
-temperature   = None
-device        = None
-mode_1_event  = None
-timeout_event = threading.Event()
+temperature_current = None
+temperature_new     = None
+device              = None
+mode_1_event        = None
+timeout_event       = threading.Event()
